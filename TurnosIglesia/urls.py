9645918427cce2629b4b8cbdbf0bec0bfd13897f
darwinLF92 <estuardo.darwinlopez@gmail.com>
2025-10-06 19:14:29 -0600

    se modifica el media en urls

--- TurnosIglesia/urls.py
+++ TurnosIglesia/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:  # Solo se sirve en modo DEBUG
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
